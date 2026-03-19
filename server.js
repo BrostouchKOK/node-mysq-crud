0c2108d8457959801462ca0e2efabbb2a6f55161
Brostouch KOK <brostouchkokit1007@gmail.com>
2026-03-19 16:38:42 +0700

    packages setup

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const colors = require('colors')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+
+//config
+dotenv.config();
 
 //rest object
 const app = express();
 
 //middleware
+app.use(express.json());
+app.use(morgan('dev'))
 
 //routes
 app.get("/test",(req,res)=>{
@@ -11,9 +19,9 @@ app.get("/test",(req,res)=>{
 })
 
 //port
-const PORT = 8080
+const PORT = process.env.PORT || 8000
 
 //listen
 app.listen(PORT, ()=>{
-    console.log("Server Running");
+    console.log(`Server Running on http://localhost:${process.env.PORT}`.bgMagenta.white);
 })
