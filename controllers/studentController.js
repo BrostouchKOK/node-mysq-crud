cea59f21d8f3ecb6b35c7d24de1f8ddd682af716
Brostouch KOK <brostouchkokit1007@gmail.com>
2026-03-20 15:24:53 +0700

    get student by id api functionality

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,29 +1,62 @@
-const db = require("../config/db")
+const db = require("../config/db");
 
 // GET ALL STUDENT LIST
-const getStudents = async (req, res)=>{
-    try {
-        const data = await db.query("SELECT * FROM students");
-        if(!data){
-            return res.status(404).send({
-                success: false,
-                message: "No Records found",
-            })
-        }
-        res.status(200).send({
-            success: true,
-            message: "All Students Records",
-            totalStudents: data[0].length,
-            data: data[0],
-        })
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({
-            success : false,
-            message: "Error in Get All Student API",
-            error,
+const getStudents = async (req, res) => {
+  try {
+    const data = await db.query("SELECT * FROM students");
+    if (!data) {
+      return res.status(404).send({
+        success: false,
+        message: "No Records found",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "All Students Records",
+      totalStudents: data[0].length,
+      data: data[0],
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Get All Student API",
+      error,
+    });
+  }
+};
+
+// GET STUDENT BY ID
+const getStudentById = async (req, res) => {
+  try {
+    const studentId = req.params.id;
+    if(!studentId){
+        return res.status(404).send({
+            success: false,
+            message: "Invalid or Provide Student id"
         })
     }
-}
+    // const data = await db.query(`SELECT * FROM students WHERE id = ?`,[studentId])
+    const data = await db.query(`SELECT * FROM students WHERE id =`+studentId)
+    if(!data){
+        return res.status(404).send({
+            success: false,
+            message: "No records found"
+        })
+    }
+    res.status(200).send({
+        success: true,
+        studentDetails: data[0],
+    })
 
-module.exports = {getStudents};
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Get Student By Id API",
+      error,
+    });
+  }
+};
+
+module.exports = { getStudents,getStudentById};
--- routes/studentsRoutes.js
+++ routes/studentsRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const { getStudents } = require('../controllers/studentController');
+const { getStudents, getStudentById } = require('../controllers/studentController');
 
 // router object
 const router = express();
 
 // routes
 
-
 // GET ALL STUDENT LIST || GET
 router.get('/get-all',getStudents);
+// GET STUDENT BY ID
+router.get("/get/:id",getStudentById);
 
 module.exports = router;
 
