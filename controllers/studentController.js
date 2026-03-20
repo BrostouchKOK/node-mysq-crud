d382a69aea29513b82981a5cbccf1b69666e9ca0
Brostouch KOK <brostouchkokit1007@gmail.com>
2026-03-20 15:54:58 +0700

    create student api functionality

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -30,25 +30,26 @@ const getStudents = async (req, res) => {
 const getStudentById = async (req, res) => {
   try {
     const studentId = req.params.id;
-    if(!studentId){
-        return res.status(404).send({
-            success: false,
-            message: "Invalid or Provide Student id"
-        })
+    if (!studentId) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid or Provide Student id",
+      });
     }
     // const data = await db.query(`SELECT * FROM students WHERE id = ?`,[studentId])
-    const data = await db.query(`SELECT * FROM students WHERE id =`+studentId)
-    if(!data){
-        return res.status(404).send({
-            success: false,
-            message: "No records found"
-        })
+    const data = await db.query(
+      `SELECT * FROM students WHERE id =` + studentId,
+    );
+    if (!data) {
+      return res.status(404).send({
+        success: false,
+        message: "No records found",
+      });
     }
     res.status(200).send({
-        success: true,
-        studentDetails: data[0],
-    })
-
+      success: true,
+      studentDetails: data[0],
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send({
@@ -59,4 +60,39 @@ const getStudentById = async (req, res) => {
   }
 };
 
-module.exports = { getStudents,getStudentById};
+const createStudent = async (req, res) => {
+  try {
+    const { name, roll_no, fees, medium } = req.body;
+    if ((!name || !roll_no, !fees, !medium)) {
+      return res.status(500).send({
+        success: false,
+        message: "Please provide all fields",
+      });
+    }
+
+    const data = await db.query(
+      "INSERT INTO students (name, roll_no, fees, medium) VALUES (?, ?, ?, ?)",
+      [name, roll_no, fees, medium],
+    );
+    if(!data){
+        return res.status(404).send({
+            success: false,
+            message: "Error in insert query",
+        })
+    }
+
+    res.status(200).send({
+        success: true,
+        message: "Student record created successfully"
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in create student API",
+      error,
+    });
+  }
+};
+
+module.exports = { getStudents, getStudentById, createStudent };
--- routes/studentsRoutes.js
+++ routes/studentsRoutes.js
@@ -1,5 +1,9 @@
-const express = require('express');
-const { getStudents, getStudentById } = require('../controllers/studentController');
+const express = require("express");
+const {
+  getStudents,
+  getStudentById,
+  createStudent,
+} = require("../controllers/studentController");
 
 // router object
 const router = express();
@@ -7,9 +11,10 @@ const router = express();
 // routes
 
 // GET ALL STUDENT LIST || GET
-router.get('/get-all',getStudents);
+router.get("/get-all", getStudents);
 // GET STUDENT BY ID
-router.get("/get/:id",getStudentById);
+router.get("/get/:id", getStudentById);
+// CREATE STUDENT || POST
+router.post("/create", createStudent);
 
 module.exports = router;
-
